2d573c30ef9201f65ffe9cd8b76fc8e6165f4516
Aayush09900 <rajaayush42@gmail.com>
2026-06-18 11:29:12 +0530

    commit

--- unquieValue.sol
+++ unquieValue.sol
@@ -7,32 +7,24 @@ contract UniqueValues {
 
     function getUniqueValues() public view returns (uint[] memory) {
         uint[] memory temp = new uint[](A.length + B.length);
-
         uint count = 0;
-
         for (uint i = 0; i < A.length; i++) {
             bool found = false;
             for (uint j = 0; j < B.length; j++) {
                 if (A[i] == B[j]) {found = true;}
             }
-
             if (!found) { temp[count] = A[i];count++;}
         }
-
         for (uint i = 0; i < B.length; i++) {
             bool found = false;
-
             for (uint j = 0; j < A.length; j++) {if (B[i] == A[j]) { found = true;}
             }
-
             if (!found) { temp[count] = B[i];count++;}
         }
-
         uint[] memory unique = new uint[](count);
         for (uint i = 0; i < count; i++) {
             unique[i] = temp[i];
         }
-
         return unique;
     }
 }
